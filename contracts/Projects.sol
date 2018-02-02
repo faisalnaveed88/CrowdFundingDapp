55f33de81781f6b54c142e561578957e6a053495
Faisal Naveed <faisalleo8@gmail.com>
2018-02-02 08:50:48 +0500

    fourth commit

--- contracts/Projects.sol
+++ contracts/Projects.sol
@@ -1,7 +1,7 @@
 pragma solidity ^ 0.4 .4;
 
 contract Projects {
-event Transfer(address indexed from, address indexed to, uint value);
+  event Transfer(address indexed from, address indexed to, uint value);
 
   struct Reward {
     uint RewardId;
@@ -11,7 +11,7 @@ event Transfer(address indexed from, address indexed to, uint value);
     string PledgeAmount;
     string EstimatedDilivery;
     string ShippingDetails;
-    
+
   }
 
   struct Story {
@@ -44,12 +44,12 @@ event Transfer(address indexed from, address indexed to, uint value);
   mapping(uint => Reward) public projectRewardList;
   address[16] public DeployerAddress;
 
-  function postProject( string _catagory, string _title, string _loucation, uint _fundingDuration, uint _fundingGoal,
+  function postProject(string _catagory, string _title, string _loucation, uint _fundingDuration, uint _fundingGoal,
     string _imageUrl, string _shortDesr, string _storyDscr, string _risk,
     string _vedioUrl) {
-      
 
-   
+
+
     var newproject = projectList[projectsCount];
     var newDetails = projectDetailsList[projectsCount];
     var newstory = projectStoriesList[projectsCount];
@@ -57,9 +57,9 @@ event Transfer(address indexed from, address indexed to, uint value);
     newproject.Catagory = _catagory;
     newproject.Title = _title;
     newproject.Loucation = _loucation;
-    newproject.ProRewardCount = 0 ;
+    newproject.ProRewardCount = 0;
     newproject.TotalGatherdAmount = 0 ether;
-    newproject.voteCount = 0 ;
+    newproject.voteCount = 0;
 
     newDetails.FundingDuration = now + _fundingDuration * 1 minutes;
     newDetails.FundingGoal = _fundingGoal;
@@ -69,21 +69,20 @@ event Transfer(address indexed from, address indexed to, uint value);
     newstory.Description = _storyDscr;
     newstory.RiskAndChallanges = _risk;
     newstory.VedioUrl = _vedioUrl;
-    
-    
+
+
 
 
   }
 
-  function getProject(uint _id) public returns(string, string, string, uint,uint ) {
-    return (projectList[_id].Catagory, projectList[_id].Title, projectList[_id].Loucation, projectList[_id].TotalGatherdAmount, projectList[_id].voteCount
-    );
+  function getProject(uint _id) public returns(string, string, string, uint, uint) {
+    return (projectList[_id].Catagory, projectList[_id].Title, projectList[_id].Loucation, projectList[_id].TotalGatherdAmount, projectList[_id].voteCount);
   }
 
 
   function getProjectDetails(uint _id) public returns(uint, uint, string, string) {
 
-    return ( projectDetailsList[_id].FundingDuration, projectDetailsList[_id].FundingGoal,projectDetailsList[_id].ImageUrl, projectDetailsList[_id].ShortDescription);
+    return (projectDetailsList[_id].FundingDuration, projectDetailsList[_id].FundingGoal, projectDetailsList[_id].ImageUrl, projectDetailsList[_id].ShortDescription);
   }
 
   function getProjectStory(uint _id) public returns(string, string, string) {
@@ -100,45 +99,47 @@ event Transfer(address indexed from, address indexed to, uint value);
     return projectsCount;
   }
 
-  function setReward(uint _proId, string _title, string _plegeAmount,string _descr, string _estimatedDilivery,string _shippingDetails ){
-    
-     projectList[_proId].ProRewardCount = projectList[_proId].ProRewardCount + 1 ;
-     var newreward = projectRewardList[projectRewardCount];
-     projectRewardCount = projectRewardCount +1 ;
-     newreward.Title = _title;
-     newreward.PledgeAmount= _plegeAmount;
-     newreward.EstimatedDilivery = _estimatedDilivery;
-     newreward.ShippingDetails = _shippingDetails;
-     newreward.projectId = _proId;
-     newreward.RewardDescription = _descr;
+  function setReward(uint _proId, string _title, string _plegeAmount, string _descr, string _estimatedDilivery, string _shippingDetails) {
+
+    projectList[_proId].ProRewardCount = projectList[_proId].ProRewardCount + 1;
+    var newreward = projectRewardList[projectRewardCount];
+    projectRewardCount = projectRewardCount + 1;
+    newreward.Title = _title;
+    newreward.PledgeAmount = _plegeAmount;
+    newreward.EstimatedDilivery = _estimatedDilivery;
+    newreward.ShippingDetails = _shippingDetails;
+    newreward.projectId = _proId;
+    newreward.RewardDescription = _descr;
 
   }
 
-  function getReward(uint _id) public returns (string, string, string, string, string){
- 
+  function getReward(uint _id) public returns(string, string, string, string, string) {
+
     return (projectRewardList[_id].Title, projectRewardList[_id].RewardDescription, projectRewardList[_id].PledgeAmount,
-            projectRewardList[_id].EstimatedDilivery, projectRewardList[_id].ShippingDetails
+      projectRewardList[_id].EstimatedDilivery, projectRewardList[_id].ShippingDetails
     );
 
   }
+
   function getCountReward(uint proId) public returns(uint) {
 
-   
+
     return projectList[proId].ProRewardCount;
   }
 
   function () payable {
-  
-   uint amount = msg.value;
-    projectList[projectsCount].TotalGatherdAmount = projectList[projectsCount].TotalGatherdAmount +  amount ;
+
+    uint amount = msg.value;
+    projectList[projectsCount].TotalGatherdAmount = projectList[projectsCount].TotalGatherdAmount + amount;
 
 
   }
 
-  function VoteProject(uint proId){
-      projectList[projectsCount].voteCount =  projectList[projectsCount].voteCount +  1 ;
-      proId=  1 ;
+  function VoteProject(uint proId) {
+    projectList[projectsCount].voteCount = projectList[projectsCount].voteCount + 1;
+    proId = 1;
 
   }
 
 }
+
